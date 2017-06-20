05f02bd6c2d5a82ee19b4521cf4f211534db6b7a
Maykon Queiroz <maykonr_queiroz@hotmail.com>
2017-06-20 02:07:18 -0300

    adição das fotos ao MapFotos

--- GuiaTuristico/app/src/main/java/com/maykon/guiaturistico/modelo/MapFotos.java
+++ GuiaTuristico/app/src/main/java/com/maykon/guiaturistico/modelo/MapFotos.java
@@ -22,5 +22,19 @@ public class MapFotos {
         map.put("2",R.drawable.n02bokas);
         map.put("3",R.drawable.n03meuescritorio);
         map.put("4",R.drawable.n04meuescritorio);
+        map.put("5",R.drawable.n05servidores);
+        map.put("6",R.drawable.n06servidores);
+        map.put("7",R.drawable.n07cic);
+        map.put("8",R.drawable.n08cic);
+        map.put("9",R.drawable.n09teatro);
+        map.put("10",R.drawable.n10teatro);
+        map.put("11",R.drawable.n11mercado);
+        map.put("12",R.drawable.n12mercado);
+        map.put("13",R.drawable.n13ponte);
+        map.put("14",R.drawable.n14ponte);
+        map.put("15",R.drawable.n15praca);
+        map.put("16",R.drawable.n16praca);
+        map.put("17",R.drawable.n17joaquina);
+        map.put("18",R.drawable.n18joaquina);
     }
 }
